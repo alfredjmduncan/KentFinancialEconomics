29e6ac7624dd0371c504e4c80fecd58f30d65d4c
alfredjmduncan <alfie.j.duncan@gmail.com>
2021-09-23 15:35:56 +0100

    Sharadar query functions added

--- src/KentFinancialEconomics.jl
+++ src/KentFinancialEconomics.jl
@@ -1,5 +1,14 @@
 module KentFinancialEconomics
 
-# Write your package code here.
+using HTTP
+using CSV
+using JSON
+using DataFrames
+using TimeSeries
+using InfoZIP
+
+export fundamentals, events, metadata, stockprices
+
+include("data_sources/quandl.jl")
 
 end
